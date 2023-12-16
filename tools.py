45aae39e90710daace52781f61acd4314984fc72
xxw <46432007+xxw1995@users.noreply.github.com>
2023-12-16 22:49:58 +0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,4 +1,5 @@
 import requests
+import json
 import io
 import base64
 import os
@@ -15,6 +16,7 @@ from langchain.base_language import BaseLanguageModel
 import re, random
 from hashlib import md5
 from utils import test_bge_cos
+from self_tools import system_info
 
 class APITool(BaseTool):
     name: str = ""
@@ -155,13 +157,48 @@ class Default_Tool(functional_Tool):
     def _call_func(self, query) -> str:
         self.get_llm_chain()
         context = "Instruction: 你是一个聊天机器人，需要根据用户的输入完成聊天任务，你需要根据下面这些背景知识来回答："
-        # 只是库
-        retrive_knowledge = test_bge_cos(query, "/data/npc_data.txt", mode="multi")
-        print("Defaurt_Tool:", retrive_knowledge)
-        context += retrive_knowledge
-        resp = self.llm_chain.predict(text=context, query=query)
-        return resp
+        # 知识库
+        retrive_knowledge = test_bge_cos(query, "data/npc_data.csv", mode="multi")
+        # retrive_knowledge不为空，则调用base_model，结合知识库输出内容
+        if retrive_knowledge != "":
+            context += retrive_knowledge
+            resp = self.llm_chain.predict(text=context, query=query)
+            return resp
+        # retrive_knowledge为空，则代表没有相似的知识，用对齐后的chat_model生成知识，再输出内容
+        # chat模型可以结合定义好的tools
+        else:
+            model_response, model_history=self.model_chat(query)
+            context += model_response
+            resp = self.llm_chain.predict(text=context, query=query)
+            return resp
 
     def get_llm_chain(self):
         if not self.llm_chain:
             self.llm_chain = LLMChain(llm=self.llm, prompt=self.prompt)
+
+    def model_chat(self, task_query):
+        from transformers import AutoTokenizer, AutoModel
+        base_dir = "models/chatglm3-6b"
+        tokenizer = AutoTokenizer.from_pretrained(base_dir, trust_remote_code=True)
+        model = AutoModel.from_pretrained(base_dir, trust_remote_code=True).cuda()
+        model = model.eval()
+        model_history = [system_info]
+        model_response, model_history = model.chat(tokenizer, task_query, history=model_history)
+        return self.run_task(model_response, model_history)
+
+    def run_task(self, model_response, model_history):
+        if isinstance(model_response, dict):
+            print("model_response is dict")
+            import function_map
+            # 使用 getattr 来从 function_map 中获取 model_response 字典中 "name" 键对应的函数。
+            func = getattr(function_map, model_response.get("name"))
+            param = model_response.get("parameters")
+            func_response = func(**param)
+            result = json.dumps(func_response, ensure_ascii=False)
+            # 把前一次的输出放入，再次调用
+            model_response, model_history = model.chat(tokenizer, result, history=model_history, role="observation")
+            # 再次执行run_task
+            model_response, model_history = run_task(model_response, model_history)
+            return model_response, model_history
+        else:
+            return model_response, model_history
